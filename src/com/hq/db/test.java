d24306fc01eec5904d8017eeb5b43a14f6420ccd
Zhangtao153 <13289265979@163.com>
2019-04-19 18:31:34 +0800

    配置 log4j

--- src/com/hq/db/test.java
+++ src/com/hq/db/test.java
@@ -1,19 +1,24 @@
 package com.hq.db;
 
+import org.apache.log4j.Logger;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+
 
 /**
  * @author zth
  * @Date 2019-04-19 16:57
  */
 public class test {
+    private static Logger log= Logger.getLogger(test.class);
     public static void main(String[] args) throws SQLException {
-        for (int i = 0; i < 10; i++) {
-            Connection connection = DB.getConnection();
-            System.out.println(connection);
-            connection.close();
-        }
+
+        log.debug("debug.........");
+        log.info("info............");
+        log.warn("warn.............");
+        log.error("error.........");
+
     }
 }
