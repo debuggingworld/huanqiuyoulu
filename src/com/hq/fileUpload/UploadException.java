43f93e4856bef8daf62f2a616a5ae8ffd3d75695
Zhangtao153 <13289265979@163.com>
2019-05-04 23:11:04 +0800

    FileException

---
+++ src/com/hq/fileUpload/UploadException.java
@@ -0,0 +1,11 @@
+package com.hq.fileUpload;
+
+/**
+ * @author zth
+ * @Date 2019-05-04 23:06
+ */
+public class UploadException extends Exception{
+    public UploadException(String msg){
+        super(msg);
+    }
+}
